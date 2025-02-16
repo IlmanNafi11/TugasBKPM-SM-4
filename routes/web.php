12372c56491619d634328f5a07261d2361b5fc98
IlmanNafi11 <ilmannafi04@gmail.com>
2025-02-16 12:47:05 +0700

    Acara 3 Dasar Routing dan Route Parameter

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,33 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+/**
+ * Mulai Acara 3
+ */
+
+Route::get('foo', function () {
+    return 'Hello World';
+});
+
+Route::get('user/{id}', fn($id)=> 'Hello ' . $id);
+
+Route::get('post/{post}/comments/{comment}', function ($postId, $commentId) {
+    //
+});
+
+// Daftar Metode
+// Route::get($uri, $callback);
+// Route::post($uri, $callback);
+// Route::put($uri, $callback);
+// Route::patch($uri, $callback);
+// Route::delete($uri, $callback);
+// Route::options($uri, $callback);
+
+Route::match(['get', 'post'], '/match', fn () => 'hello');
+
+Route::get('search/{search}', fn($search) => $search)->where('search', '.*');
+
+/**
+ * Selesai Acara 3
+ */
